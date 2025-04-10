7c32fd7641409a193c37a21c4f57bfc50b2da5a3
LucasPilatti <lucasppirani@gmail.com>
2025-04-10 08:07:01 -0400

    Update aula38.py

--- aula38.py
+++ aula38.py
@@ -27,33 +27,26 @@ Caso o contrário:
 
 O primeiro número do CPF é 7
 '''
+# Gerador simples de CPF válido:
+
 import random
+def calculo_digitos(base, multi):
+    total = 0
+    for digito in base:
+        total += int(digito) * multi
+        multi -= 1
 
-primeiros_digitos = ''
+    digito_final = (total * 10) % 11   
+    digito_final = digito_final if digito_final <= 9 else 0
+
+    base += str(digito_final)
+    return base
+
+cpf = ''
 for digito in range(9):
-    primeiros_digitos += str(random.randint(0, 9))
+    cpf += str(random.randint(0, 9))
 
-multiplicador_1 = 10
+cpf = calculo_digitos(cpf, 10)
+cpf = calculo_digitos(cpf, 11)
 
-total_1 = 0
-for digito in primeiros_digitos:
-    total_1 += int(digito) * multiplicador_1
-    multiplicador_1 -= 1
-
-digito_1 = (total_1 * 10) % 11   
-digito_1 = digito_1 if digito_1 <= 9 else 0
-
-primeiros_digitos += str(digito_1)
-multiplicador_2 = 11
-total_2 = 0
-
-for digito in primeiros_digitos:
-    total_2 += int(digito) * multiplicador_2
-    multiplicador_2 -= 1
-
-digito_2 = (total_2 * 10) % 11
-digito_2 = digito_2 if digito_2 <= 9 else 0
-
-
-resultado = primeiros_digitos + str(digito_2)
-print(resultado)
+print(cpf)
